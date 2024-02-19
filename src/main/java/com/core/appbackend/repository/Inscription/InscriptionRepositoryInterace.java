98f61e357ab0f6b22e2ca1230174888697df2083
elie-imario <elieimario1@gmail.com>
2024-02-19 10:52:50 +0300

    remove unnecessary package

--- src/main/java/com/core/appbackend/repository/Inscription/InscriptionRepositoryInterace.java
+++ src/main/java/com/core/appbackend/repository/Inscription/InscriptionRepositoryInterace.java
@@ -2,7 +2,6 @@ package com.core.appbackend.repository.Inscription;
 
 import com.core.appbackend.beans.Inscription;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface InscriptionRepositoryInterace extends CrudRepository<Inscription, Long> {
