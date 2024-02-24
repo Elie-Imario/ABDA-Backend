93c9f3d5c0f041c778474ba1e77ebc5f8d857fcf
elie-imario <elieimario1@gmail.com>
2024-02-24 22:18:19 +0300

    add audit configuration

---
+++ src/main/java/com/core/appbackend/config/AuditConfig.java
@@ -0,0 +1,21 @@
+package com.core.appbackend.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.envers.AuditReader;
+import org.hibernate.envers.AuditReaderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuditConfig {
+    private final EntityManagerFactory entityManagerFactory;
+
+    public AuditConfig(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    AuditReader auditReader() {
+        return AuditReaderFactory.get(entityManagerFactory.createEntityManager());
+    }
+}
