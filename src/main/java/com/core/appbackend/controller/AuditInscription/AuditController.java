e2ef38c61cfbdb9bd3151a18c9aaa8c3867db30b
elie-imario <elieimario1@gmail.com>
2024-02-27 08:11:21 +0300

    change revisiondetail response to list

--- src/main/java/com/core/appbackend/controller/AuditInscription/AuditController.java
+++ src/main/java/com/core/appbackend/controller/AuditInscription/AuditController.java
@@ -1,13 +1,13 @@
 package com.core.appbackend.controller.AuditInscription;
 
 import com.core.appbackend.beans.audit.AuditModelDTO;
+import com.core.appbackend.playload.model.RevisionDetail;
+import com.core.appbackend.playload.response.AuditsResponse;
 import com.core.appbackend.service.AuditInscription.AuditInscriptionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,15 +17,15 @@ import java.util.List;
 public class AuditController {
     @Autowired
     AuditInscriptionServiceImpl auditInscriptionService;
-
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/audit_inscriptions")
-    public ResponseEntity<List<?>> getAllAudit(){
+    public ResponseEntity<?> getAllAudit(){
         try{
             List<AuditModelDTO> Audits = auditInscriptionService.findAll();
             if(Audits.isEmpty()){
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
-            return new ResponseEntity<>(Audits, HttpStatus.OK);
+            return new ResponseEntity<>(new AuditsResponse(Audits, HttpStatus.OK), HttpStatus.OK);
 
         }catch (Exception e){
             e.printStackTrace();
@@ -33,11 +33,13 @@ public class AuditController {
         }
     }
 
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/RevisionTypeDetails")
-    public ResponseEntity<HashMap<String, Integer>> RevisionTypeDetails(){
+    public ResponseEntity<List<?>> RevisionTypeDetails(){
         try{
-            HashMap<String, Integer> actionType = auditInscriptionService.getRevisionTypeDetails();
-            return new ResponseEntity<>(actionType, HttpStatus.OK);
+            HashMap<String, Integer> revisionTypeDetails = auditInscriptionService.getRevisionTypeDetails();
+            List<RevisionDetail> revisionDetailList = auditInscriptionService.listRevisionDetail(revisionTypeDetails);
+            return new ResponseEntity<>(revisionDetailList, HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
