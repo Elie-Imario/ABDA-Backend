15b46dd345e87dfacb1f7fce872bc20ad6c910d8
elie-imario <elieimario1@gmail.com>
2024-02-27 08:09:42 +0300

    list revisionDetail

--- src/main/java/com/core/appbackend/service/AuditInscription/AuditInscriptionServiceImpl.java
+++ src/main/java/com/core/appbackend/service/AuditInscription/AuditInscriptionServiceImpl.java
@@ -2,6 +2,7 @@ package com.core.appbackend.service.AuditInscription;
 
 import com.core.appbackend.beans.Inscription;
 import com.core.appbackend.beans.audit.AuditModelDTO;
+import com.core.appbackend.playload.model.RevisionDetail;
 import org.hibernate.envers.AuditReader;
 import org.hibernate.envers.query.AuditEntity;
 import org.hibernate.envers.query.AuditQuery;
@@ -17,8 +18,9 @@ import java.util.List;
 public class AuditInscriptionServiceImpl {
     @Autowired
     private AuditReader auditReader;
-    private List<AuditQuery> getRevisions(){
-        AuditQuery auditQuery = auditReader.createQuery()
+
+    private AuditQuery auditQueryForRevTable(){
+        return auditReader.createQuery()
                 .forRevisionsOfEntity(Inscription.class, false,true)
                 .addProjection(AuditEntity.revisionNumber()) // [0] Revision_info (:id)
                 .addProjection(AuditEntity.revisionType()) // [1] Revision_info_type (:action_type)
@@ -29,61 +31,59 @@ public class AuditInscriptionServiceImpl {
                 .addProjection(AuditEntity.property("droitInscription")) // [6] droitInscription
                 .addProjection(AuditEntity.revisionProperty("userName")) // [7] responsable
                 .addOrder(AuditEntity.revisionProperty("timestamp").desc());
-
-        return auditQuery.getResultList();
     }
 
 
     public List<AuditModelDTO> findAll(){
-        List<AuditQuery> auditQueries = getRevisions();
+        List<AuditQuery> auditQueries = auditQueryForRevTable().getResultList();
         List<AuditModelDTO> audits = new ArrayList<>();
-        try{
-            AuditModelDTO auditModelDTO = new AuditModelDTO();
-            for(Object auditQueryObject : auditQueries){
-                Object[] auditRowArray = (Object[]) auditQueryObject;
-                if(auditRowArray[1].toString().equals("ADD")){
-                    auditModelDTO = new AuditModelDTO(
-                            Long.parseLong(auditRowArray[0].toString()),
-                            Long.parseLong(auditRowArray[3].toString()),
-                            auditRowArray[5].toString(),
-                            auditRowArray[4].toString(),
-                            null,
-                            Integer.valueOf(auditRowArray[6].toString()),
-                            new Timestamp(Long.parseLong(auditRowArray[2].toString())),
-                            auditRowArray[7].toString(),
-                            auditRowArray[1].toString()
-                    );
-                } else if (auditRowArray[1].toString().equals("DEL")) {
-                    auditModelDTO = new AuditModelDTO(
-                            Long.parseLong(auditRowArray[0].toString()),
-                            Long.parseLong(auditRowArray[3].toString()),
-                            auditRowArray[5].toString(),
-                            auditRowArray[4].toString(),
-                            Integer.valueOf(auditRowArray[6].toString()),
-                            null,
-                            new Timestamp(Long.parseLong(auditRowArray[2].toString())),
-                            auditRowArray[7].toString(),
-                            auditRowArray[1].toString()
-                    );
-                }else{
-                    auditModelDTO = new AuditModelDTO(
-                            Long.parseLong(auditRowArray[0].toString()),
-                            Long.parseLong(auditRowArray[3].toString()),
-                            auditRowArray[5].toString(),
-                            auditRowArray[4].toString(),
-                            getOldDroit(Integer.valueOf(auditRowArray[3].toString()), Long.parseLong(auditRowArray[0].toString())),
-                            Integer.valueOf(auditRowArray[6].toString()),
-                            new Timestamp(Long.parseLong(auditRowArray[2].toString())),
-                            auditRowArray[7].toString(),
-                            auditRowArray[1].toString()
-                    );
+            try{
+                AuditModelDTO auditModelDTO = new AuditModelDTO();
+                for(Object auditQueryObject : auditQueries){
+                    Object[] auditRowArray = (Object[]) auditQueryObject;
+                    if(auditRowArray[1].toString().equals("ADD")){
+                        auditModelDTO = new AuditModelDTO(
+                                Long.parseLong(auditRowArray[0].toString()),
+                                Long.parseLong(auditRowArray[3].toString()),
+                                auditRowArray[5].toString(),
+                                auditRowArray[4].toString(),
+                                null,
+                                Integer.valueOf(auditRowArray[6].toString()),
+                                new Timestamp(Long.parseLong(auditRowArray[2].toString())),
+                                auditRowArray[7].toString(),
+                                auditRowArray[1].toString()
+                        );
+                    } else if (auditRowArray[1].toString().equals("DEL")) {
+                        auditModelDTO = new AuditModelDTO(
+                                Long.parseLong(auditRowArray[0].toString()),
+                                Long.parseLong(auditRowArray[3].toString()),
+                                auditRowArray[5].toString(),
+                                auditRowArray[4].toString(),
+                                Integer.valueOf(auditRowArray[6].toString()),
+                                null,
+                                new Timestamp(Long.parseLong(auditRowArray[2].toString())),
+                                auditRowArray[7].toString(),
+                                auditRowArray[1].toString()
+                        );
+                    }else{
+                        auditModelDTO = new AuditModelDTO(
+                                Long.parseLong(auditRowArray[0].toString()),
+                                Long.parseLong(auditRowArray[3].toString()),
+                                auditRowArray[5].toString(),
+                                auditRowArray[4].toString(),
+                                getOldDroit(Integer.valueOf(auditRowArray[3].toString()), Long.parseLong(auditRowArray[0].toString())),
+                                Integer.valueOf(auditRowArray[6].toString()),
+                                new Timestamp(Long.parseLong(auditRowArray[2].toString())),
+                                auditRowArray[7].toString(),
+                                auditRowArray[1].toString()
+                        );
+                    }
+                    audits.add(auditModelDTO);
                 }
-                audits.add(auditModelDTO);
-            }
 
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
 
 
         return audits;
@@ -91,7 +91,7 @@ public class AuditInscriptionServiceImpl {
 
     private int getOldDroit(int inscriptionId, Long currentRev){
         AuditQuery auditQuery = auditReader.createQuery()
-                .forRevisionsOfEntity(Inscription.class, false,true)
+                .forRevisionsOfEntity(Inscription.class, true,true)
                 .addProjection(AuditEntity.property("droitInscription"))
                 .add(AuditEntity.id().eq(inscriptionId))
                 .add(AuditEntity.revisionNumber().lt(currentRev))
@@ -113,6 +113,21 @@ public class AuditInscriptionServiceImpl {
         }
 
         return CountRevisionType(revisionTypesArray);
+    }
+
+    public List<RevisionDetail> listRevisionDetail(HashMap<String, Integer> revisionDetails){
+        List<RevisionDetail> revisionDetailList = new ArrayList<>();
+        RevisionDetail revisionDetail = new RevisionDetail();
+        if (!revisionDetails.isEmpty()){
+            for (HashMap.Entry<String, Integer> entry : revisionDetails.entrySet()) {
+                String key = entry.getKey();
+                int value = entry.getValue();
+                revisionDetail = new RevisionDetail(key, value);
+                revisionDetailList.add(revisionDetail);
+            }
+        }
+
+        return revisionDetailList;
     }
 
 
