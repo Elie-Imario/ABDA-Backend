ebc5b0f013715af56d585caf53e5f26967478d07
elie-imario <elieimario1@gmail.com>
2024-02-28 20:30:31 +0300

    remove previous version of audit

--- src/main/java/com/core/appbackend/repository/AuditInscription/AuditInscriptionRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package com.core.appbackend.repository.AuditInscription;
-
-import com.core.appbackend.beans.Audit_inscirption;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuditInscriptionRepositoryInterface extends CrudRepository<Audit_inscirption, Long> {
-    Iterable<Audit_inscirption> findAll(Sort auditId);
-}
